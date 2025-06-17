ee4b61b53af3d242fcd9fbf9f6e789be5d129b94
Ki Rin <miriam.a.silver@gmail.com>
2025-06-17 17:47:51 +0300

    Update config.py

--- config.py
+++ config.py
@@ -7,8 +7,19 @@ class Settings:
     TOKEN_SECRET = os.getenv("TOKEN_SECRET", "dgen_secret_key")
     ALLOW_DEFAULT_TOKEN = os.getenv("ALLOW_DEFAULT_TOKEN", "true").lower() == "true"
     
+    # Server settings
+    HOST = os.getenv("HOST", "0.0.0.0")
+    PORT = int(os.getenv("PORT", "8001"))
+    
+    # CORS settings
+    ALLOWED_ORIGINS = os.getenv("ALLOWED_ORIGINS", "*").split(",")
+    
+    # Rate limiting
+    RATE_LIMIT = int(os.getenv("RATE_LIMIT", "100"))
+    MAX_CONCURRENCY = int(os.getenv("MAX_CONCURRENCY", "50"))
+    
     # Database - multiple URIs for resilience
-    MONGO_URI =  os.getenv("MONGO_URI", "mongodb://admin_mongodb:cD4uwQCF@maas-gt-d284-u2045.nam.nsroot.net:37017/?authSource=admin&readPreference=primary&ssl=true&tlsAllowInvalidCertificates=true&tlsAllowInvalidHostnames=true")
+    MONGO_URI = os.getenv("MONGO_URI", "mongodb://admin_mongodb:cD4uwQCF@maas-gt-d284-u2045.nam.nsroot.net:37017/?authSource=admin&readPreference=primary&ssl=true&tlsAllowInvalidCertificates=true&tlsAllowInvalidHostnames=true")
     MONGO_URI_BACKUP = os.getenv("MONGO_URI_BACKUP", "")
     MONGO_URI_FALLBACK = os.getenv("MONGO_URI_FALLBACK", "")
     DB_NAME = os.getenv("DB_NAME", "dgen_db")
